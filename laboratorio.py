6ca2e48588e1b5ec7014950392cf062739c8b836
romich1307 <rcamargoh@unsa.edu.pe>
2026-09-12 19:31:48 -0500

    feat: add cpu_procesos function for parallel CPU tasks using multiprocessing

--- laboratorio.py
+++ laboratorio.py
@@ -48,3 +48,22 @@ def cpu_threads(n):
 
     t1.join()
     t2.join()
+    
+def cpu_procesos(n):
+    mitad = n // 2
+
+    p1 = multiprocessing.Process(
+        target=suma_cuadrados,
+        args=(0, mitad)
+    )
+
+    p2 = multiprocessing.Process(
+        target=suma_cuadrados,
+        args=(mitad, n)
+    )
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()

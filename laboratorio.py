ec734cdbedc2baf6776e6609139ad87349e6539b
romich1307 <rcamargoh@unsa.edu.pe>
2026-09-12 19:29:56 -0500

    feat: add suma_cuadrados function to calculate sum of squares

--- laboratorio.py
+++ laboratorio.py
@@ -5,3 +5,14 @@ import statistics
 import os
 import platform
 from datetime import datetime
+# ============================================================
+# PARTE A
+# ============================================================
+
+def suma_cuadrados(inicio, fin):
+    suma = 0
+
+    for i in range(inicio, fin):
+        suma += i * i
+
+    return suma
